4cd0dd090d1fc498c0f7c9873a7c2958348e6a40
Bruno Vercelli <bruno.vercelli@hotmail.com>
2021-06-10 20:43:13 -0300

    Implementação da camera

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Dimensions, TouchableOpacity} from 'react-native';
 import {Camera} from 'expo-camera';
 
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return (
-    <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={() => alert('test')}>Home</TouchableOpacity>
-        <TouchableOpacity style={styles.button}>Afya</TouchableOpacity>
-        <TouchableOpacity style={styles.button}>Contato</TouchableOpacity>
-    </View>
-  )
+
+    const [allowCamera, setAllowCamera] = useState<any>(null);
+
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect( () => {
+        (async () => {
+            const {status} = await Camera.requestPermissionsAsync();
+            setAllowCamera(status == 'granted')
+        })();
+    }, []);
+
+    if ( allowCamera == null || false){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.paragraph}> 
+                    Acesso a camera não permitido
+                </Text>
+            </View>
+        )
+    }
+    return (
+        <View style={styles.container}>
+            <Camera style={styles.camera} type={type}>
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back ?
+                        Camera.Constants.Type.front :
+                        Camera.Constants.Type.back
+                        )
+                    }}
+                    >
+                <Text style={styles.paragraph}>Toque aqui para acessar a camera</Text>
+            </TouchableOpacity>
+            </Camera>
+            <TouchableOpacity style={styles.button}>Afya</TouchableOpacity>
+            <TouchableOpacity style={styles.button}>Contato</TouchableOpacity>
+        </View>
+    )
 }
 
 const styles =  StyleSheet.create({
@@ -26,7 +61,10 @@ const styles =  StyleSheet.create({
     },
     button: {
         flex: 0.1,
-
+    },
+    camera: {
+        height: Dimensions.get('window').height / 2,
+        width: Dimensions.get('window').width
     }
 })
 
